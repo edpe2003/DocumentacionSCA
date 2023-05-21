6c783ef2689ed00d9638df76adf0f0927c1d7b71
edpe2003 <edpe2003@gmail.com>
2023-05-21 14:48:01 -0300

    add search bar

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -9,12 +9,12 @@ function Body() {
 
   return (
     <div>
-      <div id="introduccion">{Requisitos()}</div>
+      <div id="introduccion"> {Requisitos()} </div>{" "}
       <div id="primeros-pasos">
-        {InstalarDevTools()}
-        {GenerarToken()}
-      </div>
-      <div id="comandos-gulp">{ComandosBasicos()}</div>
+        {" "}
+        {InstalarDevTools()} {GenerarToken()}{" "}
+      </div>{" "}
+      <div id="comandos-gulp"> {ComandosBasicos()} </div>{" "}
     </div>
   );
 }
@@ -22,8 +22,7 @@ function Body() {
 function Requisitos() {
   return (
     <div className="category-content" id="requisitos">
-      <p className="tittle"> Requisitos</p>
-      {InstalarNPM()}
+      <p className="tittle"> Requisitos </p> {InstalarNPM()}{" "}
     </div>
   );
 }
@@ -31,23 +30,24 @@ function Requisitos() {
 function InstalarNPM() {
   return (
     <div className="secondary-content">
-      <p className="sub-tittle">Instalar NPM</p>
+      <p className="sub-tittle"> Instalar NPM </p>{" "}
       <div className="description">
         <p>
           Para instalar npm primero debemos de instalar{" "}
-          <a href="https://nodejs.org/es/download">Node</a>, luego debemos de
+          <a href="https://nodejs.org/es/download"> Node</a>, luego debemos de
           instalar{" "}
-          <a href="https://github.com/coreybutler/nvm-windows/releases">NVM</a>{" "}
-          descargamos y ejecutamos el archivo <a>nvm-setup.exe</a>. <br />
+          <a href="https://github.com/coreybutler/nvm-windows/releases"> NVM</a>{" "}
+          descargamos y ejecutamos el archivo <a> nvm - setup.exe </a>. <br />
           Para finalizar ejecutamos el siguiente comando en la consola:
-        </p>
+        </p>{" "}
         <div className="code">
           <div className="header-code">
             <BsFillTerminalFill />
-          </div>
-          <div className="content-code">npm install -g npm@7.7.2</div>
-        </div>
-      </div>
+          </div>{" "}
+          <div className="content-code"> nvm install 18.12.1</div>{" "}
+          <div className="content-code"> nvm use 18.12.1</div>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 }
@@ -55,10 +55,8 @@ function InstalarNPM() {
 function InstalarDevTools() {
   return (
     <div className="category-content" id="instalar-dev-tools">
-      <p className="tittle">Instalar Dev TOOls</p>
-      {DescargarTheme()}
-      {DescargarExtensiones()}
-      {GenerarAmbiente()}
+      <p className="tittle"> Instalar Dev TOOls </p> {DescargarTheme()}{" "}
+      {DescargarExtensiones()} {GenerarAmbiente()}{" "}
     </div>
   );
 }
@@ -66,14 +64,14 @@ function InstalarDevTools() {
 function DescargarTheme() {
   return (
     <div className="secondary-content">
-      <p className="sub-tittle">Descargar Theme</p>
+      <p className="sub-tittle"> Descargar Theme </p>{" "}
       <div className="description">
         <p>
           Para descargar el theme debemos de descargar el archivo que
-          encontramos al buscar <a>themedev</a> en la global search del backend
-          en Netsuite
-        </p>
-      </div>
+          encontramos al buscar <a> themedev </a> en la global search del
+          backend en Netsuite{" "}
+        </p>{" "}
+      </div>{" "}
     </div>
   );
 }
@@ -81,14 +79,14 @@ function DescargarTheme() {
 function DescargarExtensiones() {
   return (
     <div className="secondary-content">
-      <p className="sub-tittle">Descargar Extensiones</p>
+      <p className="sub-tittle"> Descargar Extensiones </p>{" "}
       <div className="description">
         <p>
           Para descargar las extensiones debemos de descargar el archivo que
-          encontramos al buscar <a>extensiondev</a> en la global search del
-          backend en Netsuite
-        </p>
-      </div>
+          encontramos al buscar <a> extensiondev </a> en la global search del
+          backend en Netsuite{" "}
+        </p>{" "}
+      </div>{" "}
     </div>
   );
 }
@@ -96,19 +94,19 @@ function DescargarExtensiones() {
 function GenerarAmbiente() {
   return (
     <div className="secondary-content">
-      <p className="sub-tittle">Generar Ambiente</p>
+      <p className="sub-tittle"> Generar Ambiente </p>{" "}
       <div className="description">
         <p>
           Luego de haber descargado las dev tools debemos de extraer ambos
           archivos y ejecutar en las carpetas generadas el siguiente comando:
-        </p>
+        </p>{" "}
         <div className="code">
           <div className="header-code">
             <BsFillTerminalFill />
-          </div>
-          <div className="content-code">npm i</div>
-        </div>
-      </div>
+          </div>{" "}
+          <div className="content-code"> npm i </div>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 }
@@ -116,38 +114,46 @@ function GenerarAmbiente() {
 function GenerarToken() {
   return (
     <div className="category-content" id="generar-token">
-      <p className="tittle">Generar Token</p>
+      <p className="tittle"> Generar Token </p>{" "}
       <div className="secondary-content">
-        <p className="sub-tittle">*Debemos de estar logueados en el backend*</p>
+        <p className="sub-tittle">
+          {" "}
+          * Debemos de estar logueados en el backend *{" "}
+        </p>{" "}
         <div className="description">
           <p>
             Luego de haber ejecutado el comando anterior podemos obtener el
             theme y las extensiones de Netsuite, para hacer esto debemos de
             ejecutar los comandos:
-          </p>
+          </p>{" "}
           <div className="code">
             <div className="header-code">
               <BsFillTerminalFill />
-            </div>
-            <div className="content-code">gulp theme:fetch</div>
-            <div className="content-code">gulp extension:fetch</div>
-          </div>
+            </div>{" "}
+            <div className="content-code"> gulp theme: fetch </div>{" "}
+            <div className="content-code"> gulp extension: fetch </div>{" "}
+          </div>{" "}
           <p>
             Al ejecutar estos comandos el sistema te pedira que selecciones un
-            token, seleccionamos la opcion de <a>generar token</a> . <br />
+            token, seleccionamos la opcion de <a> generar token </a> . <br />
             Si ocurre algun error debemos de modificar el archivo{" "}
-            <a>./ns_npm_repository/oauth1/index.js</a>, buscamos{" "}
-            <a>hostnameStep1</a> y cambiamos rest por system, deberia de
-            quedar de la siguiente manera:
-          </p>
+            <a> . / ns_npm_repository / oauth1 / index.js </a>, buscamos{" "}
+            <a> hostnameStep1 </a> y cambiamos rest por system, deberia quedar
+            de la siguiente manera:
+          </p>{" "}
           <div className="code">
             <div className="header-code">
               <BsFillTerminalFill />
-            </div>
-            <img src="../../img/token.png" alt="token" />
-          </div>
-        </div>
-      </div>
+            </div>{" "}
+            <div className="code-img">
+              <img
+                src="https://raw.githubusercontent.com/edpe2003/DocumentacionSCA/develop/src/img/token.png"
+                alt="token"
+              />
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 }
@@ -155,22 +161,25 @@ function GenerarToken() {
 function ComandosBasicos() {
   return (
     <div className="category-content">
-      <p className="tittle">Comandos Basicos de Gulp</p>
+      <p className="tittle"> Comandos Basicos de Gulp </p>{" "}
       <div className="secondary-content">
         <div className="description">
           <div className="code">
             <div className="header-code">
               <BsFillTerminalFill />
-            </div>
-            <div className="content-code">gulp theme:fetch</div>
-            <div className="content-code">gulp extension:fetch</div>
-            <div className="content-code">gulp theme:local</div>
-            <div className="content-code">gulp extension:local</div>
-            <div className="content-code">gulp theme:update-manifest</div>
-            <div className="content-code">gulp extension:update-manifest</div>
-          </div>
-        </div>
-      </div>
+            </div>{" "}
+            <div className="content-code"> gulp theme: fetch </div>{" "}
+            <div className="content-code"> gulp extension: fetch </div>{" "}
+            <div className="content-code"> gulp theme: local </div>{" "}
+            <div className="content-code"> gulp extension: local </div>{" "}
+            <div className="content-code"> gulp theme: update - manifest </div>{" "}
+            <div className="content-code">
+              {" "}
+              gulp extension: update - manifest{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,43 @@
 import "../../styles/Header.css";
 import React, { useEffect, useState } from "react";
 import Category from "./Category";
-import { BsList } from "react-icons/bs";
-
-const SidebarInformation = require("../../database/sidebar.json");
+import { BsList, BsSearch } from "react-icons/bs";
 
 function Header() {
   const [showSideBar, setSideBarState] = useState(false);
+  const [SidebarInformation, setSidebarInformation] = useState(
+    require("../../database/sidebar.json")
+  );
 
   // Initialize function
   useEffect(() => {}, []);
 
   const handleClickHeaderBtn = (showSideBar) => {
     setSideBarState(showSideBar ? false : true);
+    setSidebarInformation(require("../../database/sidebar.json"));
+  };
+
+  function filterSubCategories(subcategory, text) {
+    if (!subcategory) return false;
+    return subcategory.some((sub) => {
+      return sub.name.toLowerCase().includes(text);
+    });
+  }
+
+  function getFilterCategories(text) {
+    let allCategories = require("../../database/sidebar.json");
+    let response = allCategories.sidebarComponent.filter((cat) => {
+      return (
+        cat.name.toLowerCase().includes(text) ||
+        filterSubCategories(cat.subcategory, text)
+      );
+    });
+    return { sidebarComponent: response };
+  }
+
+  const changeCategorySearch = (e) => {
+    let text = e.target.value;
+    setSidebarInformation(getFilterCategories(text.toLowerCase()));
   };
 
   return (
@@ -26,18 +51,21 @@ function Header() {
         >
           <BsList />
         </div>
-
-        <p>Documentacion SCA</p>
-      </div>
+        <p> Documentacion SCA </p>{" "}
+      </div>{" "}
       {showSideBar ? (
         <div className="all-categories">
+          <div className="block-category search-container">
+            <input type="text" onChange={changeCategorySearch} />
+            <BsSearch className="search-btn" />
+          </div>
           {SidebarInformation.sidebarComponent.map((item, index) => (
             <Category key={index} item={item} />
-          ))}
+          ))}{" "}
         </div>
       ) : (
-        <></>
-      )}
+        <> </>
+      )}{" "}
     </div>
   );
 }
